51416854429c97110c1a6ebad613ac56f85b8ba5
Mark <m1pearson@me.com>
2020-06-23 20:59:47 +0100

    Add Nature Of Periodic Orbits which has tests Add Extra functionallity to ODEAnalysis can determin nature of periodic orbit ODEAnalysis can now deal with q and p being in higher dim Add ability to find eigen values and eigen vectors of monodromy matrix in ODEAnalysis

--- ODEAnalysis.py
+++ ODEAnalysis.py
@@ -1,9 +1,8 @@
 import numpy as np
-
-import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+
 
 def examplefunc(t, x1, x2):
     f1 = x2
@@ -11,18 +10,46 @@ def examplefunc(t, x1, x2):
     f3 = 0
     return np.array([f1, f2])
 
+def ExampleWithMonoDromy(t, x, y, px, py, M):
+    L = np.matrix([[0, 0, 1, 0],
+                  [0, 0, 0, 5],
+                  [1, 0, 0, 0],
+                  [0, -5, 0, 0]])
+    
+    
+    x_dot = 1*px
+    y_dot = 5*py
+    px_dot = 1*x
+    py_dot = -5*y
+    
+    M_dot = L*M
+    
+    return np.array([x_dot, y_dot, px_dot, py_dot, M_dot])
+    
 
-class ODEPlotter():
+
+class ODEAnalysis():
+    '''Plot PhasePlane by using PlotPhasePlane()
+        Plot 3D PhaseSpace by using PlotPhaseSpace3D()
+        Calculate eingenvalues and eigenvectors of monodromy matrix by using CalcEigen
+    '''
     def __init__(self, FuncToUse=examplefunc, StepOfTime=0.01):
-        self.f = FuncToUse
+        self.standard = FuncToUse
         self.dt = StepOfTime
+        self.f = FuncToUse
 
     def RungeKutta(self, T, t_0, x_0):
         N = int(T//self.dt)
         t = np.zeros(N)
         num_x = len(x_0)
-        x = np.zeros([N, num_x])
-
+        
+        if type(x_0[0]) ==  np.ndarray or type(x_0[0]) == list:
+            x = np.zeros([N, num_x, len(x_0[0])])
+        else:
+            x = np.zeros([N, num_x], dtype=np.complex)
+        
+        
+        
         # Initial Conditions
         x[0] = x_0
         t[0] = t_0
@@ -37,7 +64,72 @@ class ODEPlotter():
             t[n+1] = t[n] + self.dt
 
         return t, x
+    
+    def CalcEigen(self, T, x_0):
+        #Calculate correct size of M(0)
+        self.f = self.MonoDromyFunc
+        I = np.identity(len(x_0)).reshape(1,-1)[0]
+        
+        t, x = self.RungeKutta(T, 0, [*x_0,*I])
+        
+        return np.linalg.eig(x[-1][len(x_0):].reshape(-1,len(x_0)))
+        
+    def getMonoDromy(self, T, x_0):
+        self.f = self.MonoDromyFunc
+        I = np.identity(len(x_0)).reshape(1,-1)[0]
+        
+        t, x = self.RungeKutta(T, 0, [*x_0,*I])
+        
+        return x[-1][len(x_0):].reshape(-1,len(x_0))
 
+        
+    def ShowEigen(self, T, x_0, IntegrateOnOrbit = False, epsilon = 1e-4):
+        I = np.identity(len(x_0)).reshape(1,-1)[0]
+        eigenvals, eigenvectors = self.CalcEigen(T,x_0)
+        for index in range(0, len(eigenvals)):
+            my_string = "Value " + str(index) + ": " + str(eigenvals[index]) + "\n"
+            my_string += "Vector " + str(index) + ": \n" 
+            for eigen_vector_component in eigenvectors[:,index]:
+                my_string += str(eigen_vector_component) + ", \n"
+            
+            if IntegrateOnOrbit:
+                #only compare direction that have been changed.
+                diff_before = abs(epsilon*eigenvectors[:,index])
+                new_x_0 = x_0 + epsilon*eigenvectors[:,index]
+                t, x = self.RungeKutta(T,0, [*new_x_0,*I])
+                final_x = x[-1][:len(x_0)]                
+                diff_after = abs(x_0 - final_x)
+                
+                my_string += "Start: " + str(new_x_0) + "\n"
+                my_string += "End: " + str(final_x)+ "\n"
+                
+                Stable = True
+                for index in range(0,len(x_0)):
+                    if diff_before[index] != 0:
+                        if 1.1*diff_after[index]>diff_before[index]: # To make sure it is definitely not stable
+                            Stable = False
+                
+                if Stable:
+                    my_string += "Asymptotically Stable in this eigenvector direction \n"
+                if not(Stable):
+                    my_string += "Not Asymptotically Stable in this eigenvector direction \n"
+                
+            
+            print(my_string)
+    
+    def MonoDromyFunc(self, t, *R):
+        #Get components and turn into matrix
+        ele = np.int(np.round((-1+(1+4*len(R))**0.5)/2))
+        new_R = np.array(R).reshape(-1,ele)
+        qp = new_R[0]
+        M = np.matrix(new_R[1:])
+        W = self.standard(t, *qp, M)
+        new_M = np.array(W[-1]).reshape(1,-1)[0]
+        new_qp = W[0:ele]
+        
+        return np.array([*new_qp, *new_M])
+        
+        
     def PlotTimeSeries(self, T, t_0, x_0, separate=False):
         t, x = self.RungeKutta(T, t_0, x_0)
 
@@ -84,7 +176,8 @@ class ODEPlotter():
         plt.xlim(*xrange)
         plt.ylim(*yrange)
         plt.show()
-
+    
+    
     def PlotPhaseSpace3D(self, xrange, yrange, zrange, T, xaxis=1, yaxis=2, zaxis=3, t_0=0, gap=0.5):
         width = np.arange(xrange[0], xrange[1]+0.01, gap)
         height = np.arange(yrange[0], yrange[1]+0.01, gap)
